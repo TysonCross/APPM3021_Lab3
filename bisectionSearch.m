84c17888492543075f861902b338eb7a2eca470a
Tyson <tyson.cross@gmail.com>
2018-03-08 10:37:17 +0200

    Initial Commit

---
+++ bisectionSearch.m
@@ -0,0 +1,9 @@
+function [ c ] = bisectionSearch( f, tol, I_0 )
+% bisectionSearch returns the root of an equation f, within tolerance tol
+% with initial bracket I_0 = [ainitial, binitial]
+
+
+% stopping criteria abs:(c_n - c_{n-1})/abs(c_n) < tol
+
+end
+
---
+++ exercise1.m
@@ -0,0 +1,6 @@
+% APPM3021 Lab 3, Exercise 1
+
+clc
+clear all
+
+
